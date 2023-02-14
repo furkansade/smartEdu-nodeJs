f7fb23cfb247aa507cd8918897ce5dcc470268cf
Furkan Sade Uckun <88970339+furkansade@users.noreply.github.com>
2023-02-15 00:06:59 +0300

    edit-dashboard-page

--- controllers/authController.js
+++ controllers/authController.js
@@ -29,7 +29,7 @@ exports.loginOneUser = async (req, res) => {
         if (same) {
           // userSession //
           req.session.userID = user._id;
-          res.status(200).redirect('/');
+          res.status(200).redirect('/users/dashboard');
         }
       });
     }
@@ -44,5 +44,13 @@ exports.loginOneUser = async (req, res) => {
 exports.logoutOneUser = (req, res) => {
   req.session.destroy(() => {
     res.redirect('/');
+  });
+};
+
+exports.getDashboardPage = async (req, res) => {
+  const user = await User.findOne({ _id: req.session.userID });
+  res.status(200).render('dashboard', {
+    pageName: 'dashboard',
+    user,
   });
 };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.route('/signup').post(authController.createOneUser); //users/signup
 router.route('/login').post(authController.loginOneUser); 
 router.route('/logout').get(authController.logoutOneUser); 
+router.route('/dashboard').get(authController.getDashboardPage); 
 
 module.exports = router;
