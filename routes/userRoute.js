d8cec09375a9e79c39c224b6017269d5c3227304
Furkan Sade Uckun <88970339+furkansade@users.noreply.github.com>
2023-02-15 00:39:45 +0300

    add-special-middlewares

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const authController = require('../controllers/authController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 router.route('/signup').post(authController.createOneUser); //users/signup
-router.route('/login').post(authController.loginOneUser); 
-router.route('/logout').get(authController.logoutOneUser); 
-router.route('/dashboard').get(authController.getDashboardPage); 
+router.route('/login').post(authController.loginOneUser);
+router.route('/logout').get(authController.logoutOneUser);
+router.route('/dashboard').get(authMiddleware, authController.getDashboardPage);
 
 module.exports = router;
+
