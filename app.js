3faf80a661bc3a8e6b642b82b228ec55049d43df
Furkan Sade Uckun <88970339+furkansade@users.noreply.github.com>
2023-02-14 10:59:53 +0300

    create-views-and-public-directory

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 
+// templateEngine //
+app.set('view engine', 'ejs');
+
+// middleWares //
+app.use(express.static('public')); // static file -> public
+
 app.get('/', (req, res) => {
-  res.status(200).send('index page!');
+  res.status(200).render('index', {
+    pageName: 'index',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.status(200).render('about', {
+    pageName: 'about',
+  });
 });
 
 const port = 3000;
